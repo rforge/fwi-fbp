23c47a132d642cf4528182219642c89a30467022
alancan <alancan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-05 12:39:26 +0000

    uppercase O1A and B
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fwi-fbp@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/fwi.fbp/R/LBtcalc.r
+++ pkg/fwi.fbp/R/LBtcalc.r
@@ -1,5 +1,5 @@
   .LBtcalc  <- function(FUELTYPE,LB,HR,CFB){                                                                                            # /* this is a new subroutine to account for the discussion in 3.6.1 */
-    alpha  <- ifelse(FUELTYPE %in% c("C1","O1a","O1b","S1","S2","S3"),
+    alpha  <- ifelse(FUELTYPE %in% c("C1","O1A","O1B","S1","S2","S3"),
         0.115,                                                                                                                         # /* page 41 */
         0.115 - 18.8 * (CFB**2.5) * exp(-8* CFB))                                                                                      # /* 72 */
   	LBt    <- (LB -1) * (1 - exp(-alpha*HR)) + 1						                                                                           # /* 81 - 2009 */
