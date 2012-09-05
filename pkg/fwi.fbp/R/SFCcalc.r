d3ff940c409599e0df10071ae115ee6f23c9a337
alancan <alancan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-05 12:43:02 +0000

    uppercase O1A and B
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fwi-fbp@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/fwi.fbp/R/SFCcalc.r
+++ pkg/fwi.fbp/R/SFCcalc.r
@@ -7,7 +7,7 @@
      SFC   <- ifelse(FUELTYPE=="C7",ifelse(FFMC>70,2*(1-exp(-0.104*(FFMC-70))),0)+1.5*(1-exp(-0.0201*BUI)),SFC)
      SFC   <- ifelse(FUELTYPE=="D1",1.5 * (1 - exp(-0.0183 * BUI)),SFC)
      SFC   <- ifelse(FUELTYPE=="M1"|FUELTYPE=="M2",PC/100*(5.0*(1-exp(-0.0115*BUI)))+((100-PC)/100*(1.5*(1-exp(-0.0183*BUI)))),SFC)
-     SFC   <- ifelse(FUELTYPE=="O1a"|FUELTYPE=="O1b",GFL,SFC)
+     SFC   <- ifelse(FUELTYPE=="O1A"|FUELTYPE=="O1B",GFL,SFC)
      SFC   <- ifelse(FUELTYPE=="S1",4.0*(1-exp(-0.025*BUI))+4.0*(1-exp(-0.034*BUI)),SFC)                                               # 19,20,25
      SFC   <- ifelse(FUELTYPE=="S2",10.0*(1-exp(-0.013*BUI))+6.0*(1-exp(-0.060*BUI)),SFC)                                              # 19,20,25
      SFC   <- ifelse(FUELTYPE=="S3",12.0*(1-exp(-0.0166*BUI))+20.0*(1-exp(-0.0210*BUI)),SFC)                                           # 19,20,25
