74afa825124f52add6db98c394862e272f95441d
alancan <alancan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-05 14:57:01 +0000

    delete useless file internalfunc.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fwi-fbp@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/fwi.fbp/R/internalfunc.R
+++
@@ -1,3 +0,0 @@
-.internalfunc  <- function(dat=NULL,output="Primary"){                                                                                           # The choice of output include: "Primarry" (default), "Secondary", and "All".
- "hi"
-}
